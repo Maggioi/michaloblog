f05dfca46c68eaa9d640fce4fe8b7c0ceef13141
Maggioi <michalmagala00@gmail.com>
2026-06-25 22:12:27 +0200

    Time to learn sqlalchemy

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,10 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, flash, redirect
 from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", title="hołm")
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash(f'Login requested for user {form.name.data}, remember_me={form.remember_me.data}')
-        return redirect(url_for('/index'))
-    return render_template("login.html", form=form)
 @app.route('/pakiernia')
 def pakiernia():
     return render_template('pakiernia.html')
@@ -21,3 +14,11 @@ def ford():
 @app.route('/przyprawy')
 def przyprawy():
     return render_template("przyprawy.html")
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'{form.username.data} intended logging in, remember me = {form.remember_me.data}')
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form)
